ecf1c782563b99df0a0c2f5ea2ea65aae07d02ed
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-29 15:56:45 +0200

    add Ui structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
+createApp({
+  data() {
+    return {
+      activeImage: 0,
+      movie: {
+        title: 'Spider Man',
+        category: 'Action',
+        duration: 60,
+        images: [
+            "./assets/img/01.webp",
+            "./assets/img/02.webp",
+            "./assets/img/03.webp",
+            "./assets/img/04.webp",
+            "./assets/img/05.webp"
+          ]
       }
     }
-  }).mount('#app')
+  },
+}).mount('#app')
